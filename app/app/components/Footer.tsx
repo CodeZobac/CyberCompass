17c8848e3928435b455809fb4d55591160db9ddd
Afonso Caboz <afonso.caboz@gmail.com>
2025-06-04 07:13:31 +0100

    Enlarged ERASMUS badge

--- app/app/components/Footer.tsx
+++ app/app/components/Footer.tsx
@@ -21,8 +21,8 @@ const Footer = () => {
                                 <Image
                                     src="/logo-footer.jpg"
                                     alt="ERASMUS Project - CyberCompass Footer Logo"
-                                    width={200}
-                                    height={200}
+                                    width={300}
+                                    height={300}
                                     className="rounded-md"
                                 />
                             </div>
